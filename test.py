1d88b1fcb67778353352bfb71c92c3b8a48c88c9
nzayeva90 <169067225+nzayeva90@users.noreply.github.com>
2024-05-09 21:01:15 +0200

    Update test.py
    made changes

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 print("Hello World!!")
 print("hello 2")
 
-
 print("other test")
-print("nuovo brench dev)")
+
 print("nuovo brench dev 2")
+print("nuova modifica che può creare problemi di merge")
+print("sono state modificate alcune cose")
