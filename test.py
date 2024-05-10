2e77c1256915d2abdc39d1881ab0bdd27a4e9ce7
nzayeva90 <n.zayeva@reply.it>
2024-05-10 10:43:02 +0200

    modifiche format

--- test.py
+++ test.py
@@ -4,13 +4,17 @@ print("hello 2")
 
 print("other test")
 
+
 def sum(x, y):
     return sum
+
+
 print(sum(9, 5))
 
 print("nuova modifica lato dev2, ho fatto le modifiche di concurrency")
 
 print("proviamo a modificare qua")
+
 
 def molt(x, y):
     return x * y
@@ -21,4 +25,3 @@ print(3, 4)
 
 print("modifica fatta su origin/master")
 print("versione 3 su origin/master")
-
