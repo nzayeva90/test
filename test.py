e57cd1be23abfc574c3be764266da6acf5bc29c4
nzayeva90 <n.zayeva@reply.it>
2024-05-10 10:09:52 +0200

    changes in master

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ print("hello 2")
 
 print("other test")
 def sum(x, y):
-    sum = x + y
-    return sum
+    return x + y
 
 
 print(sum(9, 5))
@@ -13,8 +12,7 @@ print("nuova modifica")
 
 
 def molt(x, y):
-    molt = x * y
-    return molt
+    return x * y
 
 
 print(3, 4)
@@ -22,3 +20,4 @@ print(3, 4)
 
 print("modifica fatta su origin/master")
 print("versione 3 su origin/master")
+
