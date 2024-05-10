f278d88308802f274ac3a7df2df7caddc2ee8e87
nzayeva90 <n.zayeva@reply.it>
2024-05-10 14:02:56 +0200

    fix def logic

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ print("other test")
 
 
 def sum(x, y):
-    return sum
+    return x+y
 
 
 print(sum(9, 5))
@@ -20,7 +20,7 @@ def molt(x, y):
     return x * y
 
 
-print(3, 4)
+print(molt(3, 4))
 
 
 print("modifica fatta su origin/master")
